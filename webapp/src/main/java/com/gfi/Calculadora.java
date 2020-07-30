f2b11b5d0b3d4f4e4800331ef702df1e25706e9b
manulombar <manu_lombar@hotmail.com>
2020-07-30 09:13:29 +0000

    dia4 02

--- webapp/src/main/java/com/gfi/Calculadora.java
+++ webapp/src/main/java/com/gfi/Calculadora.java
@@ -2,7 +2,7 @@ package com.gfi;
 
 public class Calculadora{
 
-    public static int suma(int a, int b){
+    public static int suma(int numero1, int numero2){
         return numero1+numero2; // No escribo el codigo
     }
     
